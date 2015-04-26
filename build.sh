256a50c8553e5cc15f9a934876ce2fbdc8812440
Artur Klesun <arturklesun@gmail.com>
2015-04-26 20:52:38 +0300

    separated build script and run script

---
+++ build.sh
@@ -0,0 +1,2 @@
+/usr/java/jdk1.8.0_40/bin/javac -sourcepath "./src" -d "./bin" "./src/Main/Main.java" -cp libs/*.jar
+
--- run.sh
+++ run.sh
@@ -1,4 +1,2 @@
-/usr/java/jdk1.8.0_40/bin/javac -sourcepath "./src" -d "./bin" "./src/Main/Main.java" -cp libs/*.jar
 cd bin
 /usr/java/jdk1.8.0_40/bin/java -cp ../libs/guava-18.0.jar:. Main.Main
-
